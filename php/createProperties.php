1757ac7e40925110e89b11ee959acd15ac06fdbf
Haoxiqiang@live.com <Haoxiqiang@live.com>
2014-07-04 12:12:21 +0800

    1.init

---
+++ php/createProperties.php
@@ -0,0 +1,25 @@
+﻿<?php
+require_once("ZhConversion.php");
+
+#print_r($zh2Hant);
+
+
+$myFile = "zh2Hant.utf8properties";
+$fh = fopen($myFile, 'w');
+
+foreach ($zh2Hant as $key => $value) {
+    fwrite($fh, $key.'='.$value."\n");
+}
+fclose($fh);
+
+$myFile = "zh2Hans.utf8properties";
+$fh = fopen($myFile, 'w');
+    
+foreach ($zh2Hans as $key => $value) {
+	fwrite($fh, $key.'='.$value."\n");
+}
+fclose($fh);    
+
+
+
+?>
